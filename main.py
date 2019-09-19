6532efbad40a25d4c4666d3c125136279282a397
Authapong Somboonwarakon <hurriken@Authapongs-MacBook-Air-3.local>
2019-09-19 13:51:59 +0700

    remove junk line

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import cv2
-import numpy as np
-import pyautogui
 import time
+import pyautogui
+import numpy as np
 
-alp="aeioulnstrdgbcmpfhvwykjxqz"
+# string of alphabet ordered which frequenly used .
+alp = "aeioulnstrdgbcmpfhvwykjxqz"
+
+# red color .
 lower = np.array([0, 0, 200])
 upper = np.array([127, 127,255])
+
 time.sleep(2)
 
+# initial alphabet 'A' into 15*15table .
 for i in range(15*15):
     pyautogui.press('a')
 
+# replace wrong slots with new alphabet .
 for i in range(1,len(alp)):
+
     img = pyautogui.screenshot('img.png')
-    time.sleep(1)
     img = cv2.imread('img.png')
-    img = cv2.resize(img,None,fx=0.5,fy=0.5)
+    # img = cv2.resize(img,None,fx=0.5,fy=0.5)
 
     result = img.copy()
     mask = cv2.inRange(img, lower, upper)
 
     _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.drawContours(img, contours, -1, (0,255,0), 3)
-    pos=[]
     for cnt in contours :
-        area = cv2.contourArea(cnt)
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(result,[box],0,(0,0,255),2)
-
         center, _, _ = cv2.minAreaRect(cnt)
         x, y = center
         pyautogui.click(int(x), int(y))
